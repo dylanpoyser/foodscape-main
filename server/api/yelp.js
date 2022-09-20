e25e6736459514b452f7e1797f227904a2972416
Dylan <dylanlovespie@gmail.com>
2022-09-20 03:26:51 -0400

    finished altering keys

--- server/api/yelp.js
+++ server/api/yelp.js
@@ -3,7 +3,8 @@ module.exports = router;
 require("dotenv").config();
 
 const yelp = require("yelp-fusion");
-const YELP_API_KEY = process.env.YELP_API_KEY;
+// const YELP_API_KEY = process.env.YELP_API_KEY;
+const YELP_API_KEY = "i_ARcXQjIleCN37-04QGL8ZPxm0kUpQydV8qFXwB4e5I1wTH6xUIIXPf-9BVwKxd6IHoLtEGwkwZjPdKEa71JbUs-wucX729H7f3jRFukdy1mWOeJyyik8QghNzsYHYx"
 const client = yelp.client(YELP_API_KEY);
 
 // POST /api/yelp
